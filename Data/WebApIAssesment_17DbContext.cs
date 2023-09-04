a37f9c426f5a89feda676b0a786973f8ff7335c1
Anjali-st <anjali90908080@gmail.com>
2023-09-04 18:58:13 +0530

    Assignment-17

---
+++ Data/WebApIAssesment_17DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApIAssesment_17.Model;
+
+namespace WebApIAssesment_17.Data
+{
+    public class WebApIAssesment_17DbContext : DbContext
+    {
+        public WebApIAssesment_17DbContext (DbContextOptions<WebApIAssesment_17DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApIAssesment_17.Model.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApIAssesment_17.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebApIAssesment_17DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApIAssesment_17DbContext") ?? throw new InvalidOperationException("Connection string 'WebApIAssesment_17DbContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
